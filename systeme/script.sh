0f41da2d679de6530150a18def6d575804ced7ea
clementbourguet <clement.bourguet@outlook.fr>
2025-04-02 14:47:54 +0200

    dernier commit

---
+++ systeme/script.sh
@@ -0,0 +1,9 @@
+echo 'Ou voulez-vous enregistrer le projet ? (Chemin absolu ex: c:/dossier)'
+read directory
+echo 'Quel est le nom de votre projet ?'
+read project
+cd 
+mkdir ze
+cd ze
+touch index.html style.css README.md
+echo 'Le projet a été ajouté avec succès.'
